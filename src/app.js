7452771aeee3ae25e36c58dbc162500d296525bb
JamGwang <twr9680@naver.com>
2024-09-23 22:01:21 +0900

    브랜치가져오기전 commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+const PORT = 3306;
+
+app.use(express.json());
+// app.use(cookieParser());
+// app.use('/api', [UsersRouter, CharactersRouter, ItemsRouter]);
+
+
+app.listen(PORT, () => {
+    console.log(PORT, '포트로 서버가 열렸어요!');
+  });
