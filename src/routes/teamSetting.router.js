cccaac94b2e6e829e122360bbd29e0d2a2e542ef
LeeSangHwee <tkdgnl56@naver.com>
2024-09-25 03:07:41 +0900

    stop

--- src/routes/teamSetting.router.js
+++ src/routes/teamSetting.router.js
@@ -4,10 +4,39 @@ import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router(); // express.Router()를 이용해 라우터를 생성합니다.
 
+// 인벤토리 불러오기
+router.get('/getInventory', authMiddleware, async (req, res) => {
+  try {
+      // 사용자id 가져오기
+      const userAccount = req.user;
+      const user = await prisma.account.findFirst({
+          where: {
+              id: userAccount.id,
+          },
+          include: {
+            PlayerInventory: {
+                  include: {
+                      Player: true // Inventory의 Player를 포함해서 가져옴
+                  }
+              }
+          }
+      });
+
+      // id 이용해서 인벤토리 가져오기
+      const result = user.PlayerInventory;
+      // 결과 응답
+      res.json({ result });
+  } catch (error) {
+      // 오류 처리
+      console.error(error);
+      res.status(500).json({ message: 'Server Error: getInventory' });
+  }
+});
+
 // 유저 ID를 통해 인벤토리의 선수 카드를 팀으로 편성하거나 교체하기
 router.post('/setPlayerTeam', authMiddleware, async (req, res) => {
     try {
-        const { playerName, replacePlayerId } = req.body; // 요청에서 카드 이름과 교체할 선수 ID를 받음
+        const { playerName, playerId } = req.body; // 요청에서 카드 이름과 교체할 선수 ID를 받음
         const userAccount = req.user;
 
         // 사용자와 팀 정보 가져오기
@@ -26,18 +55,16 @@ router.post('/setPlayerTeam', authMiddleware, async (req, res) => {
             }
           }
         });
-        
+
         // 편성할 카드가 인벤토리에 있는지 확인
-        const selectCard = user.PlayerInventory.Player.find(player => player.name === playerName);
-        if (!selectCard) {
-            return res.status(404).json({ message: '선수를 찾을 수 없습니다.' });
-        }
-        console.log(selectCard);
+        const selectCard = user.PlayerInventory.find(element => element.Player.name === playerName);
+        if (!selectCard) return res.status(404).json({ message: '선수를 찾을 수 없습니다.' });
+  
         // 팀에 3명의 선수가 있는지 확인
-        if (user.Team.Player.length < 3) {
+        if (user.Team.length < 3) {
             // 팀에 자리가 남아있으면 선수 추가
             await prisma.team.update({
-              where: { id: user.Team.teamId },
+              where: { teamId: user.Team.teamId },
               data: {
                 Player: {
                   connect: { playerId: selectCard.playerId }, // 새로운 선수를 팀에 추가
@@ -47,13 +74,11 @@ router.post('/setPlayerTeam', authMiddleware, async (req, res) => {
         } else {
             // 교체할 선수를 선택
             const replacePlayer = user.Team.Player.find(player => player.playerId === replacePlayerId);
-            if (!replacePlayer) {
-                return res.status(404).json({ message: '교체할 선수가 팀에 없습니다.' });
-            }
+            if (!replacePlayer) return res.status(404).json({ message: '교체할 선수가 팀에 없습니다.' });
 
             // 교체 로직 (선수를 제거하고 새로운 선수 추가)
             await prisma.team.update({
-              where: { id: user.Team.teamId },
+              where: { teamId: user.Team.teamId },
               data: {
                 Player: {
                   disconnect: { playerId: replacePlayerId }, // 기존 선수 제거
